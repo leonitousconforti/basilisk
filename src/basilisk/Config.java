134d39005890b48a2e6685b50fd714888486fa2f
leo conforti <leo.conforti@tgeagle.org>
2020-03-12 20:18:29 -0400

    config variables

---
+++ src/basilisk/Config.java
@@ -0,0 +1,8 @@
+package basilisk;
+
+public class Config {
+    public static int screenConfig = 0;
+
+    public static final int SCREEN_1440x900 = 2;
+    public static final int SCREEN_1920x1080 = 1;
+}
