af5fa49ae63ae1faf400f1d9dd6dfcbc6a3a917a
leo conforti <leo.conforti@tgeagle.org>
2020-03-17 13:17:16 -0400

    Update Config.java

--- src/basilisk/Config.java
+++ src/basilisk/Config.java
@@ -3,6 +3,30 @@ package basilisk;
 public class Config {
     public static int screenConfig = 0;
 
+    public static final int GuiFrameRate = 10;
     public static final int SCREEN_1440x900 = 2;
     public static final int SCREEN_1920x1080 = 1;
+
+    public static int loadingStep = 0;
+    public static boolean loading = false;
+
+    public static final int loadingStepDone = 0;
+    public static final int loadingStepApple = 1;
+    public static final int loadingStepSnake = 2;
+    public static final int loadingStepCornerLeft = 3;
+    public static final int loadingStepCornerRight = 4;
+
+    public static volatile boolean paused = false;
+    public static boolean showGameDetection = false;
+
+    public static boolean showAiDebugs = false;
+    public static boolean showAnimationDebugs = false;
+
+    public final void saveConfig() {
+
+    }
+
+    public final void loadConfig() {
+
+    }
 }
